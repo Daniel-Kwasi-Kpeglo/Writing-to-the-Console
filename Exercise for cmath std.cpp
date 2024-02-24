dad243c76a0f5ca45cbab699b52dc796a88a3189
Daniel Kwasi Kpeglo (Ph.D.) <90644507+Daniel-Kwasi-Kpeglo@users.noreply.github.com>
2024-02-24 12:57:01 +0200

    Add files via upload

---
+++ Exercise for cmath std.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+  cout << "Enter radius: ";
+
+  double radius = pow(radius, 2);
+
+  const double pi = 3.142;
+
+  cin >> radius;
+
+  double area = pi * radius; 
+
+  cout << area;
+
+  return 0; 
+
+}
---
+++ Using the cmath Standard library.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+  double result = pow(2, 4);
+  cout << result;
+
+  return 0;
+
+
+
+}
